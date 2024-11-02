4a4d284b25c6a5b21933c61163a97220079d632a
Tharuka Sooriyaarachchi <tharuka@maximimpressions.com>
2024-11-02 16:24:27 +0530

    added androd cap

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'navoda.fusion.lumiraq.com',
+  appName: 'Navoda Sales',
+  webDir: 'dist/navoda/browser',
+
+};
+
+export default config;
